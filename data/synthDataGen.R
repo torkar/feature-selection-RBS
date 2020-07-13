72cedbe8ff146ef281285c9a14547991749ac4d0
Richard Torkar <richard.torkar@gmail.com>
2020-07-13 12:41:57 +0200

    synthetic data gen update

--- data/synthDataGen.R
+++ data/synthDataGen.R
@@ -1,18 +1,14 @@
 library(synthpop)
+library(openxlsx)
 
 # load empirical data set
 d <- read.xlsx("data/Features.xlsx", sheet = "Features")
 
-# synthesis data
-synthData <- syn(d)
+# keep only 5% of the rows
+size = nrow(d) - (nrow(d) * 0.95)
+d <- d[sample(nrow(d), size), ]
 
-# extract the synthesized data and store it in d
-d <- data.frame(synthData$syn)
-
-# How does it look like?
-str(d)
-
-# Make factors ordered or set 1/0 for binary vars
+# Make factors ordered 
 d$State <- factor(d$State, 
                   levels = c("Elicited, Dropped", 
                              "Elicited, Prio, Dropped", 
@@ -44,11 +40,30 @@ d$Architects.involvement <- factor(d$Architects.involvement,
                                               "Joint Design"), 
                                    ordered = TRUE)
 
+
+### data synthesis
+
+# data synthesis 
+# * sample is regular sampling
+# * polr is for ordered categorical (multinomial)
+# * cart for the rest of the data types
+method.ini <- c("sample","polr","cart","cart","polr","polr","cart","cart",
+                "cart","polr")
+
+synthData <- syn(d, method = method.ini)
+
+# Check and see that the empirical data is comparable to synthetic data
+compare(synthData, d) 
+
+# extract the synthesized data and store it in d
+d <- data.frame(synthData$syn)
+
+## Recode so it's configured as empirical data
 # binary
 d$Critical.feature <- ifelse(d$Critical.feature == 'Yes', 1, 0)
 d$Dependency <- ifelse(d$Dependency == 'Yes', 1, 0)
 
-# next, standardize where appropriate and create shorter var names
+# next, standardize where appropriate
 d$prio_s <- scale(d$Team.priority)
 d$sh_s <- scale(d$Stakeholders)
 d$kc_s <- scale(d$Key.customers)
@@ -68,3 +83,5 @@ e <- readRDS("data/data.rds")
 
 # Check that it worked
 stopifnot(identical(d,e) == TRUE)
+
+
